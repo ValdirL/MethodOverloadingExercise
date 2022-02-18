a21b8dcace49b753257b545324ee4d082c6ae9c4
Valdir Lima <valdirlima508@gmail.com>
2022-02-18 13:42:53 -0500

    first commit

---
+++ MethodOverloadingExercise/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace MethodOverloadingExercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        
+        public static int Add(int x, int y)
+        {
+            return x + y;
+        }
+
+        public static decimal Add(decimal x, decimal y)
+        {
+            return x + y;
+        }
+
+        public static string Add(int x, int y, bool z)
+        {
+            var sum = x + y;
+
+            if (z == true && sum > 1)
+            {
+                return $"{sum} dollars";
+            }
+            else if (z == true && sum == 1)
+            {
+                return $"{sum} dollar";
+            }
+            else if (z == true && sum < 1)
+            {
+                return $"{sum} dollars";
+            }
+            else
+            {
+                return $"{sum} dollars";
+            }
+
+        }
+        
+
+    }
+}
